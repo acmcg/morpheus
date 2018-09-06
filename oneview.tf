dea0ab118684efea9b3e94c78151ac4836a8ad8d
McGregor <andrew.mcgregor@hpe.com>
2018-09-06 16:10:50 +0800

    Message

--- oneview.tf
+++ oneview.tf
@@ -1,16 +1,21 @@
+variable "profile_name1" {}
+variable "profile_name2" {}
+variable "OneView_password" {}
+
+
 provider "oneview" {
   ov_username   = "andrew"
-  ov_password   = ""
+  ov_password   = "${var.OneView_password}"
   ov_endpoint   = "https://oneview4.host.local"
 }
-resource "oneview_server_profile" "default" {
-  name = "terraform-server-profile"
+resource "oneview_server_profile" "app" {
+  name = "${var.profile_name1}"
   template = "Automation Template"
-#  hardware_uri:   "" => "<computed>"
-#  ilo_ip:         "" => "<computed>"
-#  public_mac:     "" => "<computed>"
-#  public_slot_id: "" => "<computed>"
-#  type:           "" => "ServerProfileV5"
+  power_state = "on"
+}
 
-
+resource "oneview_server_profile" "db" {
+  name = "${var.profile_name2}"
+  template = "Automation Template"
+  power_state = "on"
 }
